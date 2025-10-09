8a260b75da6addfb534389db8493c6e501494862
skpatel <153447817+Sandip-364710@users.noreply.github.com>
2025-10-09 11:29:06 +0530

    Update settings.py

--- my_portfoliowebsite/my_portfoliowebsite/settings.py
+++ my_portfoliowebsite/my_portfoliowebsite/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-@#s^op6pa)^yc=!kixracbq@v4a@esfu1tiw0t%t#+1)%0zu@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-CSRF_TRUSTED_ORIGINS = ["https://sandip-website-kjb4.onrender.com"]
-ALLOWED_HOSTS  = ["sandip-website-kjb4.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://sandip-website-8jdn.onrender.com"]
+ALLOWED_HOSTS  = ["sandip-website-8jdn.onrender.com"]
 # ALLOWED_HOSTS = ["*"]
 
 # Application definition
